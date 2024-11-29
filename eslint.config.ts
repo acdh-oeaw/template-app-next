ee4772f4765806622a598d092e738f9197a80548
Stefan Probst <stefanprobst@posteo.de>
2024-11-29 12:25:42 +0100

    test: add storybook

--- eslint.config.ts
+++ eslint.config.ts
@@ -3,6 +3,7 @@ import nextConfig from "@acdh-oeaw/eslint-config-next";
 import nodeConfig from "@acdh-oeaw/eslint-config-node";
 import playwrightConfig from "@acdh-oeaw/eslint-config-playwright";
 import reactConfig from "@acdh-oeaw/eslint-config-react";
+import storybookConfig from "@acdh-oeaw/eslint-config-storybook";
 // import tailwindcssConfig from "@acdh-oeaw/eslint-config-tailwindcss";
 import gitignore from "eslint-config-flat-gitignore";
 // @ts-expect-error Missing type declaration.
@@ -16,6 +17,7 @@ const config: Config = [
 	...nextConfig,
 	// ...tailwindcssConfig,
 	...playwrightConfig,
+	...storybookConfig,
 	{
 		plugins: {
 			// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
