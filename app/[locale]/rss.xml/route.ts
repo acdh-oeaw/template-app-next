85aba82fde44dbd3b955b2dca84f63985f147b13
Stefan Probst <stefanprobst@posteo.de>
2024-12-01 09:09:37 +0100

    feat: add rss feed

---
+++ app/[locale]/rss.xml/route.ts
@@ -0,0 +1,20 @@
+import type { Locale } from "@/config/i18n.config";
+import { createFeed } from "@/lib/create-feed";
+
+export const dynamicParams = false;
+
+interface Context {
+	params: {
+		locale: Locale;
+	};
+}
+
+export async function GET(request: Request, context: Context): Promise<Response> {
+	const { params } = context;
+
+	const { locale } = params;
+
+	const feed = await createFeed(locale);
+
+	return new Response(feed, { headers: { "content-type": "application/xml" } });
+}
