21d6f28e395b753df5935460dad80827d1c12d2e
Stefan Probst <stefanprobst@posteo.de>
2025-02-17 09:31:41 +0100

    refactor: prefer generate-image-metadata in opengraph image generation

--- app/[locale]/opengraph-image.tsx
+++ app/[locale]/opengraph-image.tsx
@@ -4,21 +4,30 @@ import { getTranslations } from "next-intl/server";
 import { MetadataImage } from "@/components/metadata-image";
 import type { Locale } from "@/config/i18n.config";
 
+const size = {
+	height: 630,
+	width: 1200,
+};
+
 interface OpenGraphImageProps {
 	params: {
 		locale: Locale;
 	};
 }
 
-// export const alt = ''
-
-export const size = {
-	width: 1200,
-	height: 630,
-};
+export function generateImageMetadata(_props: OpenGraphImageProps) {
+	return [
+		{
+			alt: "",
+			contentType: "image/png",
+			id: "global",
+			size,
+		},
+	];
+}
 
 export default async function OpenGraphImage(
-	props: Readonly<OpenGraphImageProps>,
+	props: Readonly<OpenGraphImageProps & { id: string }>,
 ): Promise<ImageResponse> {
 	const { params } = props;
 
