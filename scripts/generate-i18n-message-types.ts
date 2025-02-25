7739785e126b4db9c0c9f6c192d32e03fd4686cf
stefanprobst <stefanprobst@posteo.de>
2025-02-25 12:02:50 +0100

    chore: remove workaround for i18n types (#45)
    should be removed once we have a way to programmatically generate i18n
    message types with `next-intl`. see
    https://github.com/amannn/next-intl/discussions/1737

---
+++ scripts/generate-i18n-message-types.ts
@@ -0,0 +1,19 @@
+import { log } from "@acdh-oeaw/lib";
+import createI18nPlugin from "next-intl/plugin";
+
+/**
+ * Generate i18n message types so they are available when running lint and typecheck *before*
+ * the next.js cli has been invoked.
+ *
+ * This script should be run as a `postinstall` or `prepare` npm lifecycle script.
+ *
+ * @see https://github.com/amannn/next-intl/discussions/1737
+ */
+createI18nPlugin({
+	experimental: {
+		/** @see https://v4.next-intl.dev/docs/workflows/typescript#messages-arguments */
+		createMessagesDeclaration: ["./content/en/metadata/index.json", "./messages/en.json"],
+	},
+})();
+
+log.success("Successfully generated i18n message types.");
