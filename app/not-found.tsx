d7d469700eb46c645c0262ccab07b515980014d6
Stefan Probst <stefanprobst@posteo.de>
2025-03-06 16:34:59 +0100

    chore: format

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -51,7 +51,7 @@ export default async function NotFoundPage(): Promise<ReactNode> {
 
 				<MainContent className="layout-grid min-h-full bg-fill-weaker">
 					<section className="grid place-content-center place-items-center py-16 xs:py-24">
-						<h1 className="text-balance text-center font-heading text-display font-strong text-text-strong">
+						<h1 className="text-center font-heading text-display font-strong text-balance text-text-strong">
 							{t("title")}
 						</h1>
 					</section>
