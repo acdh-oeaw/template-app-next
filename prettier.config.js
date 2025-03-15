eebc0baa1475c1da8935d10a627ffc60f186139d
Stefan Probst <stefanprobst@posteo.de>
2025-03-15 07:45:49 +0100

    chore: import tailwind prettier plugin for better typescript support in options

--- prettier.config.js
+++ prettier.config.js
@@ -1,11 +1,12 @@
 /** @typedef {import("prettier").Config} Config */
 
 import preset from "@acdh-oeaw/prettier-config";
+import * as tailwindcssPlugin from "prettier-plugin-tailwindcss";
 
 /** @type {Config} */
 const config = {
 	...preset,
-	plugins: [...(preset.plugins ?? []), "prettier-plugin-tailwindcss"],
+	plugins: [...(preset.plugins ?? []), tailwindcssPlugin],
 	tailwindFunctions: ["cn", "styles"],
 	tailwindStylesheet: "./styles/index.css",
 };
