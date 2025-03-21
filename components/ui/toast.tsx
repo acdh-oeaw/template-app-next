8b7939f617a7f84b5a6ad3f776c98c486dce0cef
Stefan Probst <stefanprobst@posteo.de>
2025-03-21 16:23:01 +0100

    style: update toasts

--- components/ui/toast.tsx
+++ components/ui/toast.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { cn } from "@acdh-oeaw/style-variants";
+import { type GetVariantProps, styles } from "@acdh-oeaw/style-variants";
 import { XIcon } from "lucide-react";
 import type { ReactNode } from "react";
 import {
@@ -14,10 +14,31 @@ import {
 } from "react-aria-components";
 import { flushSync } from "react-dom";
 
-export interface ToastContent {
+const toastStyles = styles({
+	base: "flex max-w-150 gap-x-3 rounded-2 border border-l-4 p-6 shadow-overlay focus-visible:focus-outline",
+	variants: {
+		tone: {
+			error: "border-stroke-error-weak border-l-stroke-error-strong bg-fill-error-weak",
+			warning: "border-stroke-warning-weak border-l-stroke-warning-strong bg-fill-warning-weak",
+			success: "border-stroke-success-weak border-l-stroke-success-strong bg-fill-success-weak",
+			information:
+				"border-stroke-information-weak border-l-stroke-information-strong bg-fill-information-weak",
+			neutral: "border-stroke-neutral-weak border-l-stroke-neutral-strong bg-fill-neutral-weak",
+			brand: "border-stroke-brand-weak border-l-stroke-brand-strong bg-fill-brand-weak",
+			"inverse-neutral":
+				"border-transparent border-l-stroke-inverse-strong bg-background-inverse **:[slot=description]:text-text-inverse-weak **:[slot=title]:text-text-inverse-strong",
+			"inverse-brand":
+				"border-transparent border-l-stroke-inverse-strong bg-fill-brand-strong **:[slot=description]:text-text-inverse-weak **:[slot=title]:text-text-inverse-strong",
+		},
+	},
+	defaults: {},
+});
+
+type ToastStyleProps = GetVariantProps<typeof toastStyles>;
+
+export interface ToastContent extends ToastStyleProps {
 	title: string;
 	description?: string;
-	status: "error" | "success";
 }
 
 export const toasts = new AriaToastQueue<ToastContent>({
@@ -40,11 +61,10 @@ export function Toast(props: Readonly<ToastProps>): ReactNode {
 	return (
 		<AriaToast
 			{...rest}
-			className={composeRenderProps(className, (className) => {
-				return cn(
-					"flex items-center gap-x-6 rounded-2 bg-fill-brand-strong px-6 py-4 text-text-inverse-strong shadow-overlay focus-visible:focus-outline",
-					className,
-				);
+			className={composeRenderProps(className, (className, renderProps) => {
+				const { toast } = renderProps;
+
+				return toastStyles({ className, tone: toast.content.tone });
 			})}
 			style={{
 				// @ts-expect-error @see https://developer.chrome.com/blog/view-transitions-update-io24#view-transition-class
@@ -53,16 +73,16 @@ export function Toast(props: Readonly<ToastProps>): ReactNode {
 			}}
 			toast={toast}
 		>
-			<AriaToastContent className="grid min-w-0 flex-1 gap-y-1">
-				<AriaText className="text-small font-strong text-text-inverse-strong" slot="title">
+			<AriaToastContent className="flex min-w-0 flex-1 flex-col gap-y-1">
+				<AriaText className="text-heading-4 font-strong text-text-strong" slot="title">
 					{toast.content.title}
 				</AriaText>
-				<AriaText className="text-small text-text-inverse-weak empty:hidden" slot="description">
+				<AriaText className="text-small text-text-weak empty:hidden" slot="description">
 					{toast.content.description}
 				</AriaText>
 			</AriaToastContent>
 			<AriaButton
-				className="interactive inline-grid shrink-0 place-content-center rounded-full text-icon-inverse hover:hover-overlay focus-visible:focus-outline pressed:press-overlay"
+				className="interactive inline-grid shrink-0 place-content-center rounded-2 text-icon-neutral hover:hover-overlay focus-visible:focus-outline pressed:press-overlay"
 				slot="close"
 			>
 				<XIcon aria-hidden={true} className="size-6 shrink-0" data-slot="icon" />
